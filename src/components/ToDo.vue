515e59c2cb2f7c73bec13f5e4290dfaec12dd73f
morningglory1996 <acer080428@outlook.jp>
2020-09-01 17:38:21 +0900

    練習追加

---
+++ src/components/ToDo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+    <h1>Todoリスト</h1>
+    <FormToDo @addToDoChild="addToDoParent"></FormToDo>
+    <ToDoList
+      :toDos="toDos"
+      @removeToDoChild="removeToDoParent"
+      @completeToDoChild="completeToDoParent"
+    ></ToDoList>
+  </div>
+</template>
+
+<script>
+import FormToDo from "./FormToDo";
+import ToDoList from "./ToDoList";
+
+export default {
+  name: "ToDo",
+  data() {
+    return {
+      toDos: [],
+    };
+  },
+  methods: {
+    addToDoParent(value) {
+      if (value) {
+        this.toDos.unshift({ text: value, complete: false });
+      }
+    },
+    removeToDoParent(index) {
+      this.toDos.splice(index, 1);
+    },
+    completeToDoParent(index) {
+      this.toDos[index].complete = !this.toDos[index].complete;
+    },
+  },
+  components: {
+    FormToDo,
+    ToDoList,
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  margin: auto;
+  text-align: center;
+  padding-top: 20px;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container>
+    <b-row class="mt-2" v-for="(toDo, index) in toDos" :key="index">
+      <b-col cols="8" :class="['text-left', {line: toDo.complete}]">
+        <h5>{{ toDo.text }}</h5>
+      </b-col>
+      <b-col cols="4" class="text-right">
+        <b-button
+          @click="completeToDoChild(index)"
+          :variant="toDo.complete ? 'success' : 'danger'"
+        >{{ toDo.complete ? '完了' : '未完了' }}</b-button>
+        <b-button style="margin-left: 5px;" @click="removeToDoChild(index)">削除</b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    toDos: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    removeToDoChild(index) {
+      this.$emit("removeToDoChild", index);
+    },
+    completeToDoChild(index) {
+      this.$emit("completeToDoChild", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.line {
+  text-decoration: line-through;
+}
+</style>
